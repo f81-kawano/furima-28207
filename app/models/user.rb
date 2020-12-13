2391b41ee2029e41861cd4a1eb79215111e1f6c3
f81-kawano <f81.kawano.keiya@gmail.com>
2020-12-13 13:28:02 +0900

    Revert "バリデーション変更１"
    This reverts commit c26cb4eadebf4c398b693fbd348f9082b3ba91da.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,12 @@
 class User < ApplicationRecord
-  has_many :items
-  has_many :buys
+  validates :nickname, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  with_options presence: true do
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
-    validates :nickname, length: { maximum: 40 }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
-    validates :birthday
-  end
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 end
