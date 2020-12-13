2694b4bc55622eef6148f3483a65176f4a1036d7
f81-kawano <f81.kawano.keiya@gmail.com>
2020-12-13 13:29:19 +0900

    userバリデーション変更１

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
-  validates :nickname, presence: true
+  has_many :items
+  has_many :buys
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+    validates :nickname, length: { maximum: 40 }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
+    validates :birthday
+  end
 end
