e681fc95c4cc5372277372bb5cf2d404d7b467d5
f81-kawano <f81.kawano.keiya@gmail.com>
2020-12-15 20:49:54 +0900

    items/newコントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,19 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:content)
+  end
 end
