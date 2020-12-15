4c416b5fd38db730771bff562879068bf6717d19
f81-kawano <f81.kawano.keiya@gmail.com>
2020-12-15 20:47:20 +0900

    itemモデル編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :buy
+
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }
+    validates :infomation, length: { maximum: 1000 }
+    validates :category_id
+    validates :status_id
+    validates :shipping_id	
+    validates :prefecture_id	
+    validates :shipping_date_id	
+    validates :price
+    validates :user
+  end
 end
