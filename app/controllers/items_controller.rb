a71c1d6f0e4145f873bedcedf1af4459c3b62b99
f81-kawano <f81.kawano.keiya@gmail.com>
2021-01-04 11:08:52 +0900

    item_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,4 +26,5 @@ class ItemsController < ApplicationController
           .permit(:image, :name, :infomation, :category_id, :status_id, :shipping_id, :prefecture_id, :shipping_date_id, :price)
           .merge(user_id: current_user.id)
   end
+  
 end
