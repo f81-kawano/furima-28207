f8384962ca9787edeba1a79665d2b9780c815e1c
f81-kawano <f81.kawano.keiya@gmail.com>
2021-01-06 15:12:37 +0900

    buy:model編集

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,2 +1,12 @@
 class Buy < ApplicationRecord
+  belongs_to :user, optional: true
+  belongs_to :item, optional: true
+  has_one :address
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :prefecture_id
+  end
 end
