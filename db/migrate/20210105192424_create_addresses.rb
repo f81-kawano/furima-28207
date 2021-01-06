6aa03e6b319579e4705946bb32c7be61080be12b
f81-kawano <f81.kawano.keiya@gmail.com>
2021-01-06 15:10:01 +0900

    address:DB作成

---
+++ db/migrate/20210105192424_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.integer    :postal_code,    null: false
+      t.integer    :prefecture_id,  null: false
+      t.integer    :city,           null: false
+      t.integer    :house_num,      null: false
+      t.integer    :building    
+      t.integer    :pthone_num      null: false
+      t.references :buy,            null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
