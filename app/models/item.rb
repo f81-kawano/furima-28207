393fae96400a69060cd25e375729b8466839b187
f81-kawano <f81.kawano.keiya@gmail.com>
2021-01-07 06:19:35 +0900

    pricr:バリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,9 @@ class Item < ApplicationRecord
     validates :image
     validates :name, length: { maximum: 40 }
     validates :infomation, length: { maximum: 1000 }
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
   with_options numericality: { other_than: 1, message: 'Select' } do
     validates :category_id
     validates :status_id
