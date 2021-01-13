1ea3f417479926eb88a54e6626355343ac09dd95
f81-kawano <f81.kawano.keiya@gmail.com>
2021-01-13 22:12:11 +0900

    formオブジェクト

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,12 +1,7 @@
 class Buy < ApplicationRecord
-  belongs_to :user, optional: true
-  belongs_to :item, optional: true
+  belongs_to :user
+  belongs_to :item
   has_one :address
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
-
-  with_options numericality: { other_than: 1, message: 'Select' } do
-    validates :prefecture_id
-  end
+  
 end
