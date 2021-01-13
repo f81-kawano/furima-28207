b820b52fb1724c486f05d48824515d46b31c4fbb
f81-kawano <f81.kawano.keiya@gmail.com>
2021-01-13 22:18:26 +0900

    buyコントローラー作成

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,9 +1,27 @@
 class BuysController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+
   def index
-    @buys = Buy.all
+    @item = Item.find(params[:item_id])
+    @buy_address = BuyAddress.new
   end
 
-  def show
-    @buys = Buy.all
+  def create
+    @item = Item.find(params[:item_id])
+    @buy_address = BuyAddress.new(buy_params)
+    if @buy_address.valid?
+      @buy_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def buy_params
+    params.require(:buy_address)
+          .permit(:postal_code, :prefecture_id, :city, :house_num, :building, :phone_num)
+          .merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
