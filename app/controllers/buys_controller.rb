e08335c6c9e307cfec623e7bb9d057b3df12866e
f81-kawano <f81.kawano.keiya@gmail.com>
2021-01-14 13:29:58 +0900

    モデル:購入者＝出品者はトップ画面

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,19 +1,23 @@
 class BuysController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    else 
+      render :index
+    end
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new(buy_params)
     if @buy_address.valid?
       @buy_address.save
       redirect_to root_path
     else
-      render action: :index
+      render :index
     end
   end
 
@@ -24,4 +28,11 @@ class BuysController < ApplicationController
           .permit(:postal_code, :prefecture_id, :city, :house_num, :building, :phone_num)
           .merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+
+
 end
