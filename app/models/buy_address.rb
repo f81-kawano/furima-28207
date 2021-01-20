5abbda29e627261150d7de9fa4fb1e065c8712fe
f81-kawano <f81.kawano.keiya@gmail.com>
2021-01-21 07:21:07 +0900

    トークンのバリデーション追加

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -3,7 +3,10 @@ class BuyAddress
   attr_accessor :token, :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_num, :building, :phone_num
   VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}\d{4}|\d{2}\d{3}|\d{3}\d{2}|\d{4}\d{1})\d{4}\z|\A0[5789]0\d{4}\d{4}\z/
 
-  validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
+  with_options presence: true do
+    validates :token
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
+  end
   validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
   with_options presence: true do
     validates :city
@@ -13,7 +16,7 @@ class BuyAddress
 
   def save
     buy = Buy.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_num: house_num, building: building, phone_num: phone_num, buy_id: buy_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_num: house_num, building: building, phone_num: phone_num, buy_id: buy.id)
   end
 
 end
