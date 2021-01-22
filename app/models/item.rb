b38143b406a2c12da6199ed444087014164b4bbb
f81-kawano <f81.kawano.keiya@gmail.com>
2021-01-22 20:51:27 +0900

    空のcommit

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,7 @@ class Item < ApplicationRecord
     validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   end
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  
   with_options numericality: { other_than: 1, message: 'Select' } do
     validates :category_id
     validates :status_id
